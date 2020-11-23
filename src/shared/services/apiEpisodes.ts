f7ce3c1c27fb6acb717385699d01fd9c2c034d20
Guilherme Oliveira <guilherme.oliveira@dtidigital.com.br>
2020-11-22 23:08:32 -0300

    remove comments

--- src/shared/services/apiEpisodes.ts
+++ src/shared/services/apiEpisodes.ts
@@ -1,19 +1,11 @@
 import axios from 'axios'
 
-// const allOrigins = ' https://api.allorigins.win/raw?url='
-
 const episodesApi = 'https://react-challenge-api.herokuapp.com/first-challenge/episodes'
 
 const url = episodesApi;
 
 const apiEpisodes = axios.create({
   baseURL: url,
-  // headers: {
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Access-Control-Allow-Credentials': true,
-  //   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Content-Encoding, Accept',
-  //   'Access-Control-Allow-Methods': 'OPTIONS, GET, POST, PATCH, PUT, DELETE'
-  // }
 })
 
 export default apiEpisodes
